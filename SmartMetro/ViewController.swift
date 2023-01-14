a6ce2fa3c9ee4cddc85c5f307ff19cfe9005f0a8
tjdrb3807 <junsung3807@naver.com>
2023-01-14 17:06:54 +0900

    Project 생성 및 git 연동

---
+++ SmartMetro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartMetro
+//
+//  Created by 전성규 on 2023/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
